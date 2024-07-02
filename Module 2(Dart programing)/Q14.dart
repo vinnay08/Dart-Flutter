08f23af8c4ce0c4db6812a0e5e21236d8ac3fb22
vinnay08 <vinayjadav6578@gmail.com>
2024-07-02 14:20:37 +0530

    module 2

--- Module 2(Dart programing)/Q14.dart
+++ Module 2(Dart programing)/Q14.dart
@@ -1,8 +1,8 @@
-//Q-14. Write a program to find the Max number from the given three 
-// number using Ternary Operator 
-
+//Q-14. Write a program to find the Max number from the given three
+// number using Ternary Operator
 
 import 'dart:io';
+
 void main(List<String> args) {
   print("Enter A : ");
   int a = int.parse(stdin.readLineSync()!);
@@ -10,5 +10,6 @@ void main(List<String> args) {
   int b = int.parse(stdin.readLineSync()!);
   print("Enter C : ");
   int c = int.parse(stdin.readLineSync()!);
-  
+  int m = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+  print("Largest number is $m");
 }
